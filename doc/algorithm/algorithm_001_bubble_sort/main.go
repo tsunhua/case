65a8ea78cfe4f0319b17dc9ec38f9d6b4c35c8e4
Lshare <lshare@MacBook-Pro.local>
2021-01-24 10:03:01 +0800

    Update bubble sort

--- doc/algorithm/algorithm_001_bubble_sort/main.go
+++ doc/algorithm/algorithm_001_bubble_sort/main.go
@@ -14,6 +14,20 @@ func bubble(arr []int) {
 	}
 	n := len(arr)
 	for i := 0; i < n; i++ {
+		for j := n - 1; j > i; j-- {
+			if arr[j] < arr[j-1] {
+				arr[j], arr[j-1] = arr[j-1], arr[j]
+			}
+		}
+	}
+}
+
+func bubble2(arr []int) {
+	if arr == nil {
+		return
+	}
+	n := len(arr)
+	for i := 0; i < n; i++ {
 		for j := 0; j < n-i-1; j++ {
 			if arr[j] > arr[j+1] {
 				arr[j], arr[j+1] = arr[j+1], arr[j]
